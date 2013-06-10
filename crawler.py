ce0d2f44aaef2ff65c47f8deb2f2ae6bd65ea348
jam <jam@mintyjam.(none)>
2013-06-10 13:18:53 -0700

    Added list structure to SiteMap

--- crawler.py
+++ crawler.py
@@ -3,6 +3,19 @@ import re
 import urllib2
 
 website = input('Enter full website domain as quoted string: ')
-links =  BeautifulSoup(urllib2.urlopen(website)).findAll('a')
-for l in links:
-    print l
+#List representation of SiteMap
+hierarchy = [[website]]
+linklist = []
+
+def getLinks(website):
+    links =  BeautifulSoup(urlliab2.urlopen(website)).findAll('a')
+    for l in links:
+        linklist.append(l)
+
+getLinks(website)
+for item in linklist:
+    print item
+    if "href" in item:
+        print item
+
+print getLinks
