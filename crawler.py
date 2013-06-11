02aaf2e8bbe71978bdf93454825a2b2b85578e7f
jam <jam@mintyjam.(none)>
2013-06-11 16:57:18 -0700

    Updated

--- crawler.py
+++ crawler.py
@@ -8,6 +8,7 @@ uniqueURLS = []
 #Return list of clean string urls from given clean string website url
 def getLinks(website):
     hierarchy = [website]
+    uniqueURLS.append(website)
     linklist = []
     links =  BeautifulSoup(urllib2.urlopen(website)).findAll('a')
     for l in links:
@@ -20,7 +21,8 @@ def getLinks(website):
     #Clean up the results 
     for item in linklist:
         #Clean up the results to only external webpages
-        if '.' in str(item) and item not in uniqueURLS:
+        if '.' in str(item) and (item not in uniqueURLS):
+            print "Got one!" + item
             hierarchy.append(str(item))
             uniqueURLS.append(str(item))
     print hierarchy
@@ -29,18 +31,24 @@ def getLinks(website):
 #Recursively go through list and generate hierarchy of urls as lists
 def getLinksOfLinks(webList):
     for sub1 in range(len(webList)):
+        print "working on " 
         if type(webList[sub1]) == list:
             webList[sub1] = getLinksOfLinks(webList[sub1])
         elif type(webList[sub1]) == str:
             print "Hit a url"
             if website in webList[sub1]:
-                if website == webList[sub1] or website in uniqueURLS:
+                if website == webList[sub1] or (webList[sub1] in uniqueURLS):
+                    print "Skipping this one: " + webList[sub1]
                     continue
-                print "In an intenal link. Following... " + webList[sub1]
-                webList[sub1] = [webList[sub1]]
-                webList[sub1].extend(getLinks(webList[sub1][0]))
+                else:    
+                    print "In an intenal link. Following... " + webList[sub1]
+                    webList[sub1] = [webList[sub1]]
+                    webList[sub1].extend(getLinks(webList[sub1][0]))
+            else:
+                print "External link. Continuing" + webList[sub1]
+                continue
         else:
-            print "Error: Parse Error"
+            print "Error"
         print "**** UPDATED WEB LIST  ****"
         prettyprint(webList)
         print "**** END UPDATED ****"
